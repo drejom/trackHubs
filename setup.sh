824b7fb4fb9782853d229ea84a86c461829b155d
Denis OMeally <omeally@gmail.com>
2020-05-20 15:35:38 -0700

    hubDirectory example

---
+++ setup.sh
@@ -0,0 +1,3 @@
+# setup.sh
+# http://genome.ucsc.edu/goldenPath/help/hubQuickStart.html
+#wget -r --no-parent --reject "index.html*" -nH --cut-dirs=3 http://genome.ucsc.edu/goldenPath/help/examples/hubDirectory/
